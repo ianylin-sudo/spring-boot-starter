94359675a17a97e36b2f1d54707677954245bad5
Lambert Lin <lambertlin@outlook.com>
2021-04-20 19:52:38 -0400

    added application.yml and actuator

--- src/main/java/com/example/springstarter/SpringStarterApplication.java
+++ src/main/java/com/example/springstarter/SpringStarterApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringStarterApplication {
 
+  // Visit localhost:8080/actuator/health to check service health
   public static void main(String[] args) {
     SpringApplication.run(SpringStarterApplication.class, args);
   }
