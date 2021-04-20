65ed57a1d71350d2b321c8fce59816aec5644c6b
Lambert Lin <lambertlin@outlook.com>
2021-04-20 17:29:23 -0400

    added application.yml

--- src/main/java/com/example/springstarter/controller/UserController.java
+++ src/main/java/com/example/springstarter/controller/UserController.java
@@ -1,41 +1,32 @@
 package com.example.springstarter.controller;
 
+import com.example.springstarter.model.User;
 import io.swagger.annotations.Api;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Api(tags = "User related APIs")
 @RequestMapping("/user")
 public class UserController {
 
-  //  @PostMapping("/")
-  //  @ApiOperation("添加用户的接口")
-  //  @ApiImplicitParams({
-  //    @ApiImplicitParam(name = "username", value = "用户名", defaultValue = "李四"),
-  //    @ApiImplicitParam(name = "address", value = "用户地址", defaultValue = "深圳", required = true)
-  //  })
-  //  public RespBean addUser(String username, @RequestParam(required = true) String address) {
-  //
-  //    return new RespBean();
-  //  }
-  //
-  //  @GetMapping("/")
-  //  @ApiOperation("根据id查询用户的接口")
-  //  @ApiImplicitParam(name = "id", value = "用户id", defaultValue = "99", required = true)
-  //  public User getUserById(@PathVariable Integer id) {
-  //
-  //    User user = new User();
-  //
-  //    user.setId(id);
-  //
-  //    return user;
-  //  }
-  //
-  //  @PutMapping("/{id}")
-  //  @ApiOperation("根据id更新用户的接口")
-  //  public User updateUserById(@RequestBody User user) {
-  //
-  //    return user;
-  //  }
+  @GetMapping("/")
+  @ApiOperation("Searching by user id")
+  @ApiImplicitParam(name = "id", value = "User Id", defaultValue = "99", required = true)
+  public User getUserById(@PathVariable Integer id) {
+
+    User user = new User();
+    user.setId(id);
+
+    return user;
+  }
+
+  @PutMapping("/{id}")
+  @ApiOperation("根据id更新用户的接口")
+  public User updateUserById(@RequestBody User user) {
+
+    return user;
+  }
 }
--- src/main/java/com/example/springstarter/model/User.java
+++ src/main/java/com/example/springstarter/model/User.java
@@ -6,14 +6,25 @@ import io.swagger.annotations.ApiModelProperty;
 @ApiModel
 public class User {
 
-  @ApiModelProperty(value = "用户id")
+  @ApiModelProperty(value = "User Id")
   private Integer id;
 
-  @ApiModelProperty(value = "用户名")
+  @ApiModelProperty(value = "Username")
   private String username;
 
-  @ApiModelProperty(value = "用户地址")
-  private String address;
+  public Integer getId() {
+    return id;
+  }
 
-  // getter/setter
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
 }
